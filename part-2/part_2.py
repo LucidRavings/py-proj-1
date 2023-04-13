b52c5728882b7615064214c160bdb9ed2f04cd48
LucidRavings <cybernerd82@yahoo.com>
2023-04-13 14:50:23 -0600

    Completed part 3

--- part-2/part_2.py
+++ part-2/part_2.py
@@ -1,31 +1,31 @@
 ### Step 1 - Lists ###
 
 # Fill in this list with several authors you are a fan of. At least 7 or 8 should do.
-my_authors = []
+my_authors = ["Christopher Paolini","J. K. Rowling","J. R. R. Tolkien","George R. R. Martin","Dave Ramsey","Michael Malice","Dr. Seuss","Orson Scott Card"]
 
 # Now let's add a new author to the end with the .append() method. Type your code below.
 
 # Code here
 # Example: my_authors.append("H.G. Wells")
-
+my_authors.append("Ed Greenwood")
 
 # Now let's remove an element in the list
 
 # Code here
 # Example: my_authors.remove("H.G. Wells")
-
+my_authors.remove("Dave Ramsey")
 
 # Now access an element by it's index. (Remember it indexes start at 0.)
 
 # Code here
 # Example: my_authors[2]
-
+my_authors[3]
 
 # Now slice the list.
 
 # Code here
 # Example: my_authors[1:4]
-
+my_authors[0:4]
 
 ### Step 2 - Tuples ###
 
@@ -33,6 +33,7 @@ my_authors = []
 
 # Code here
 # Example: my_author_tuple = ("F. Scott Fitzgerald", "J.K. Rowling", "Ernest Hemingway", "Emily Dickenson", "George Orwell", "Ray Bradbury")
+my_authors_tuple = ("Christopher Paolini","J. K. Rowling","J. R. R. Tolkien","George R. R. Martin","Dave Ramsey","Michael Malice","Dr. Seuss","Orson Scott Card")
 
 
 ### Step 3 - Sets ###
@@ -41,19 +42,21 @@ my_authors = []
 
 # Code here
 # Example: my_author_set = {"F. Scott Fitzgerald", "J.K. Rowling", "Ernest Hemingway", "Emily Dickenson", "George Orwell", "Ray Bradbury"}
+my_authors_set = {"Christopher Paolini","J. K. Rowling","J. R. R. Tolkien","George R. R. Martin","Dave Ramsey","Michael Malice","Dr. Seuss","Orson Scott Card"}
 
 
 # Add a new author to your set.
 
 # Code here
 # Example: my_author_set.add("J.R.R. Tolkien")
-
+my_authors_set.add("Glenn Beck")
 
 # Try adding the same author again, and be sure to print your set.
 
 # Code here
 # Example: my_author_set.add("J.R.R. Tolkien")
-
+my_authors_set.add("Glenn Beck")
+print(my_authors_set)
 
 
 ### Step 4 - For Loops ###
@@ -72,3 +75,15 @@ my_authors = []
 # for book in my_authors_set:
     # print(book)
 
+for author in my_authors:
+    print(author)
+
+print("--------")
+
+for author in my_authors_tuple:
+    print(author)
+
+print("--------")
+
+for author in my_authors_set:
+    print(author)
--- part-3/part_3.py
+++ part-3/part_3.py
@@ -1,5 +1,27 @@
+my_library = [
+   {"title": "The Hunger Games",
+    "author": "Suzanne Collins",
+    "year": 2008,
+    "rating": 4.32,
+    "pages": 374
+    },
+   {"title": "Ender's Game",
+    "author": "Orson Scott Card",
+    "year": 1985,
+    "rating": 4.41,
+    "pages": 324
+    },
+   {"title": "Eragon",
+    "author": "Christopher Paolini",
+    "year": 2002,
+    "rating": 5.00,
+    "pages": 544
+    }
+]
+
+
 my_book = {
-    "title": "Then Hunger Games",
+    "title": "The Hunger Games",
     "author": "Suzanne Collins",
     "year": 2008,
     "rating": 4.32,
@@ -9,15 +31,75 @@ my_book = {
 # Follow the instructions in this part of the project. Define and flesh out your function below, which should accept a dictionary as an argument when called, and return a string of the info in that book-dictionary in a user-friendly readable format.
 
 # Code below
+def book_parser(book_dictionary):
+    print(book_dictionary)
+    title = book_dictionary["title"]
+    author = book_dictionary["author"]
+    year = book_dictionary["year"]
+    pages = book_dictionary["pages"]
+    rating = book_dictionary["rating"]
+    book_string = f"{title} was written by {author} in {year}.  Is {pages} pages long, and has a rating of {rating}"
+    return book_string
+    
 
-
-
+print(book_parser(my_book))
 
 # Once you are finished with that function, create several more functions which return individual pieces of information from the book.
 
 # Code below
+def book_name(book_dictionary):
+    return book_dictionary["title"]
 
+def book_author(book_dictionary):
+    return book_dictionary["author"]
 
-# Finally, create at least three new functions that might be useful as we expand our home library app. Perhaps thing of a way you could accept additional arguments when the function is called? Also, imagine you have a list filled with dictionaries like above.
+def book_year(book_dictionary):
+    return book_dictionary["year"]
+
+def book_pages(book_dictionary):
+    return book_dictionary["pages"]
+
+def book_rating(book_dictionary):
+    return book_dictionary["rating"]
+
+print(book_name(my_book))
+print(book_author(my_book))
+print(book_year(my_book))
+print(book_pages(my_book))
+print(book_rating(my_book))
+
+# Finally, create at least three new functions that might be useful as we expand our home library app. Perhaps think of a way you could accept additional arguments when the function is called? Also, imagine you have a list filled with dictionaries like above.
 
 # Code below
+
+#Func 1: Count of all books in the library.
+
+def library_book_count(book_library):
+    book_count = 0
+    for book_dictionary in book_library:
+        book_count += 1
+    
+    return f"There are {book_count} books in this library"
+
+print(library_book_count(my_library))
+#Func 2: List the names of all books.
+
+def get_set_of_books(book_library):
+    book_name_set = set()
+
+    for book_dictionary in book_library:
+        book_name_set.add(book_dictionary["title"])
+
+    return f"This library contains {book_name_set}"
+
+print(get_set_of_books(my_library))
+#Func 3: List the names of all books below a given page count.
+def get_books_by_page(book_library, num):
+    book_set = set()
+    for book_dictionary in book_library:
+        if book_dictionary["pages"] < num:
+            book_set.add(book_dictionary["title"])
+        
+    return f"Books with less than {num} pages in this library {book_set}"
+
+print(get_books_by_page(my_library,500))
