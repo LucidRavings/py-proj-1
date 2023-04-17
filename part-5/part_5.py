d708887a4dd6cd40f1f959406534af3e8ea36b50
LucidRavings <cybernerd82@yahoo.com>
2023-04-16 19:59:35 -0600

    Completed part_5

--- part-5/part_5.py
+++ part-5/part_5.py
@@ -4,6 +4,31 @@
 
 # Code here
 
+my_library = "library.txt"
+
+def add_book(library):
+    title = input("Title of your book:")
+    author = input("Author of your book:")
+
+    try:
+        year = int(input("The year the book was published:"))
+    except:
+        year = int(input("Use only numbers please:"))
+
+    try:
+        rating = float(input("Rate this book with a number from 1 to 5:"))
+    except:
+        rating = float(input("Use only numbers please:"))
+
+    try:
+        pages = int(input("Number of pages in this book:"))
+    except:
+        pages = int(input("Use only numbers please:"))
+
+    with open(library, "a") as f:
+        f.write(f"{title}, {author}, {year}, {rating}, {pages}\n")
+
+# add_book(my_library)
 
 ### Step 2 - Read data from a .txt
 
@@ -11,6 +36,16 @@
 
 # Code here
 
+def print_book_list(library):
+    with open(library, "r") as f:
+        file = f.readlines()
+
+        for line in file:
+            title, author, year, rating, pages = line.split(", ")
+
+            print(f"{title} by {author} | published in {year} | {rating}/5.0 | {pages} pages long.")
+
+# print_book_list(my_library)
 
 ### Step 3 - if __name__ == "__main__":
 
@@ -23,3 +58,60 @@
 
 ## Now follow the instructions in this final step. Expand your project. Clean up the code. Make your application functional. Great job getting your first Python application finished!
 
+def print_book_count(library):
+    count = 0
+    with open(library, "r") as f:
+        file = f.readlines()
+
+        for line in file:
+            count += 1
+    print(f"There are {count} books in this library")
+
+def print_total_page_count(library):
+    count = 0
+    with open(library, "r") as f:
+        file = f.readlines()
+
+        for line in file:
+            title, author, year, rating, pages = line.split(", ")
+            page_number = int(pages)
+            count += page_number
+    print(f"There are {count} pages in your library")
+
+def print_last_book(library):
+    book = ""
+    with open(library, "r") as f:
+        file = f.readlines()
+
+        for line in file:
+            title, author, year, rating, pages = line.split(", ")
+
+            book = (f"{title} by {author} | published in {year} | {rating}/5.0 | {pages} pages long.")
+    print(book)
+
+def main_menu(library):
+
+    waiting = True
+
+    while waiting:
+        option = input("\nType 1 to add a book to your library.\nType 2 to list all books in your library.\nType 3 to count the number of books in this library.\nType 4 to count the combined total of all books in your library.\nType 5 to see your most recently added book.\nType 0 to quit.\nType here:")
+
+        if option == "1":
+            add_book(library)
+            print("\nBook added!")
+        elif option == "2":
+            print_book_list(library)
+        elif option == "3":
+            print_book_count(library)
+        elif option == "4":
+            print_total_page_count(library)
+        elif option == "5":
+            print_last_book(library)
+        elif option == "0":
+            print("\nCome back soon!")
+            waiting = False
+        else:
+            print("Failed to run any options, please type a valid number")
+
+if __name__ == "__main__":
+    main_menu(my_library)
